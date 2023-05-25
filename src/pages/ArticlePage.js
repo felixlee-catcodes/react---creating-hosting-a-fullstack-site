15e029b4dd1c9d1ad524a52df56327bb89adafe2
C. Felix Lee <felixlee.music@gmail.com>
2023-05-25 18:58:18 -0400

    URL parameters with react router

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,7 +1,18 @@
+import { useParams } from "react-router-dom";
+import articles from "./article-content";
+
 const ArticlePage = () => {
-    return (
-        <h1>This is the article page!</h1>
-    );
-}
+  const { articleId } = useParams();
+  const article = articles.find((article) => article.name === articleId);
+  return (
+    <>
+      <h1>{article.title}</h1>
+
+      {article.content.map((el) => (
+        <p>{el}</p>
+      ))}
+    </>
+  );
+};
 
 export default ArticlePage;
---
+++ src/pages/article-content.js
@@ -0,0 +1,94 @@
+const articles = [
+  {
+    name: "moon-in-aquarius",
+    title: "Unveiling the Unique Personality Traits of an Aquarius Moon Sign",
+    content: [
+      `Introduction:
+In astrology, the Moon sign represents our emotional nature and innermost self. When someone has their Moon sign in Aquarius, it adds an intriguing layer of individuality and quirkiness to their personality. People born with an Aquarius Moon are known for their progressive thinking, intellectual curiosity, and humanitarian spirit. Let's explore some of the prominent personality traits associated with this captivating lunar placement.`,
+
+      `1. Independent and Free-Spirited:
+Individuals with an Aquarius Moon possess an inherent need for freedom and independence. They value their autonomy and cherish their personal space. They often display an unconventional streak, enjoying non-conformity and originality. These individuals are drawn to unique ideas, innovative concepts, and progressive philosophies that challenge societal norms.`,
+
+      `2. Intellectual and Analytical:
+Aquarius Moons are highly intellectual and possess a natural curiosity. They have a keen interest in expanding their knowledge and often excel in intellectual pursuits. Their minds are analytical, objective, and geared towards problem-solving. They possess an innate ability to think outside the box, often approaching challenges with inventive and inventive solutions.`,
+
+      `3. Humanitarian and Visionary:
+Aquarius Moons are driven by a deep sense of social justice and have a strong desire to make a positive impact on the world. They possess a compassionate and empathetic nature, often involving themselves in humanitarian causes and advocating for equality and fairness. Their visionary outlook enables them to see the bigger picture and envision a better future for humanity.`,
+
+      `4. Unconventional and Eccentric:
+Uniqueness is a defining trait of an Aquarius Moon. They embrace their individuality and are unafraid to express themselves authentically. Their unconventional nature may manifest in various ways, whether through their style, interests, or social circles. They often gravitate towards avant-garde art, unconventional lifestyles, and offbeat hobbies.`,
+
+      `5. Friendly and Open-Minded:
+Aquarius Moons are renowned for their friendly and approachable nature. They genuinely value social connections and enjoy engaging with people from diverse backgrounds. Their open-mindedness and accepting attitude allow them to create harmonious and inclusive environments. They are great listeners and offer valuable insights to those seeking their advice.`,
+
+      `6. Detached and Emotionally Reserved:
+While Aquarius Moons are compassionate, they can also exhibit a certain emotional detachment. They tend to analyze their feelings rather than let them overpower their rationality. This detached nature allows them to remain objective and make impartial judgments. However, it's essential for them to balance their logical approach with acknowledging and nurturing their emotional well-being.`,
+
+      `Conclusion:
+Individuals with an Aquarius Moon possess a captivating blend of intellect, individuality, and humanitarianism. Their quest for personal freedom, open-mindedness, and progressive thinking make them valuable contributors to society. While they may appear emotionally reserved, their compassionate and empathetic nature shines through in their dedication to creating positive change. Embracing their unique qualities, Aquarius Moon individuals inspire others to challenge conventions and explore the vast possibilities of the human spirit.`,
+    ],
+  },
+  {
+    name: "leo-rising-sun-opposite",
+    title: "Unveiling the Dynamic Persona of a Leo Rising with Sun Opposition",
+    content: [
+      `Introduction:
+Astrology offers valuable insights into our personalities, shedding light on the complex interplay of different signs in our birth charts. When a person has their Rising sign in Leo, combined with a Sun opposition aspect, a fascinating and dynamic personality emerges. In this article, we'll explore the multifaceted traits and energies that shape the individuality of someone with this astrological combination.`,
+
+      `Radiant Presence and Charismatic Aura:
+A person with Leo as their Rising sign exudes a natural radiance and possesses a captivating presence. They command attention effortlessly, drawing others towards them like moths to a flame. Their confident demeanor and magnetic charisma make them stand out in a crowd. They have an innate ability to light up a room and leave a lasting impression on everyone they encounter.`,
+
+      `Creative Expression and Dramatic Flair:
+With a Leo Rising, individuals possess a flair for creativity and self-expression. They have a deep appreciation for the arts and often find joy in engaging in theatrical pursuits. Whether it's through acting, writing, or any form of artistic expression, they thrive when they can showcase their talents to captivate and entertain others. Their creativity is fueled by a passionate desire to express themselves authentically and leave a lasting impact on the world.`,
+
+      `Confidence and Self-Assurance:
+Leo Rising individuals exude an unwavering self-confidence that can be inspiring to those around them. They are comfortable in their own skin and have a strong belief in their abilities. This self-assurance allows them to take risks and pursue their ambitions fearlessly. They have a natural leadership quality, effortlessly assuming positions of authority and guiding others towards success.`,
+
+      `Attention-Seeking and Need for Recognition:
+Due to their Leo Rising influence, individuals with this combination often have a strong need for recognition and validation. They thrive on being noticed and appreciated for their talents and accomplishments. They strive to be the center of attention, relishing opportunities to showcase their abilities. Their hunger for applause and admiration can sometimes drive them to work even harder to achieve their goals.`,
+
+      `Challenges in Balancing Individuality and Relationships:
+The opposition aspect of the Sun adds an intriguing layer to the Leo Rising persona. While their Leo energy craves individuality and self-expression, the Sun opposition may create inner conflicts when it comes to relationships and partnerships. Finding the balance between asserting their unique identity and compromising for the sake of harmony can be a recurring theme for individuals with this aspect.`,
+
+      `Generosity and Warmth:
+Underneath their charismatic exterior, Leo Rising individuals possess a warm and generous heart. They genuinely care about others and enjoy showering their loved ones with affection and attention. Their loyal and protective nature makes them natural defenders of their friends and family, fiercely standing up for those they hold dear.`,
+
+      `Conclusion:
+A Leo Rising with Sun Opposition combination brings forth a captivating personality marked by confidence, creativity, and a need for recognition. These individuals radiate charm and possess a natural ability to command attention. While they may face challenges in balancing their individuality with relationships, their warmth, generosity, and leadership qualities make them both inspiring and captivating figures. Embracing their unique blend of traits, individuals with this astrological combination have the potential to leave an indelible mark on the world, shining brightly as the true stars they are.`,
+    ],
+  },
+  {
+    name: "sun-in-aquarius-7th-house",
+    title: "Aquarius Sun in the 7th House: Unconventional Connections and Intellectual Harmony",
+    content: [
+      `Introduction:
+The placement of the Sun in the 7th house in astrology brings forth a fascinating blend of Aquarius energy and relationship dynamics. Individuals born with their Sun in Aquarius in the 7th house possess a unique set of personality traits that influence their approach to partnerships, friendships, and their overall social interactions. Let us delve into the depths of their intriguing characteristics.`,
+
+      `1. Independent Thinkers:
+Aquarius Sun in the 7th house individuals are renowned for their independent and unconventional thinking. They possess a strong sense of individuality and are often driven by their progressive ideas and ideals. They value intellectual stimulation and seek partners who can engage in thought-provoking conversations.`,
+
+      `2. Humanitarian Outlook:
+With their innate empathy and concern for humanity, these individuals possess a strong humanitarian streak. They strive for equality and fairness in their relationships, often championing social causes. Their desire to make a positive impact on the world is reflected in their approach to partnerships, where they seek harmony and cooperation based on shared values.`,
+
+      `3. Intellectual Stimulation:
+For those with an Aquarius Sun in the 7th house, mental connection is of utmost importance. They are attracted to partners who stimulate their intellect, challenge their perspectives, and encourage personal growth. Engaging in deep discussions and exchanging knowledge are integral aspects of their relationships.`,
+
+      `4. Open-Mindedness:
+Aquarius Sun in the 7th house individuals have an open-minded and accepting nature. They are comfortable with unconventional lifestyles, alternative viewpoints, and unique experiences. Their partners often find them non-judgmental and appreciative of diverse opinions, making them excellent friends and confidantes.`,
+
+      `5. Eccentricity and Uniqueness:
+Unconventionality and eccentricity are often key traits associated with individuals having this placement. They possess a distinct sense of style, taste, and expression, and they encourage their partners to embrace their own individuality. They value authenticity and appreciate partners who bring their own unique flavor to the relationship.`,
+
+      `6. Need for Personal Space:
+Despite their desire for connection, those with an Aquarius Sun in the 7th house also cherish their personal space. They require freedom and independence to pursue their interests and maintain a healthy balance between togetherness and autonomy. Their partners must understand and respect this need for occasional solitude.`,
+
+      `7. Future-Oriented Approach:
+Individuals with this Sun placement have a forward-thinking mindset. They are visionaries who are constantly contemplating the future and exploring innovative ideas. They appreciate partners who share their aspirations and can contribute to their dreams, creating a sense of shared purpose and collaboration.`,
+
+      `Conclusion:
+The combination of an Aquarius Sun in the 7th house signifies a unique and intellectually-driven approach to relationships. These individuals seek partners who not only appreciate their unconventional nature but also provide intellectual stimulation and share their vision for a better world. By embracing their open-mindedness, humanitarian spirit, and need for personal growth, Aquarius Sun in the 7th house individuals can foster deep and meaningful connections that transcend societal norms.`,
+    ],
+  },
+];
+
+export default articles;
