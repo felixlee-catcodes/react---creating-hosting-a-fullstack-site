0570af80bb2e0b2b861d5d98c303aaf74453493e
C. Felix Lee <felixlee.music@gmail.com>
2023-05-25 19:48:50 -0400

    creating and linking the articles list, added a back button on article pg

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,18 +1,23 @@
-import { useParams } from 'react-router-dom';
-import articles from './article-content';
+import { useParams } from "react-router-dom";
+import articles from "./article-content";
+import { IoIosArrowRoundBack } from "react-icons/io";
+import { Link } from "react-router-dom";
 
 const ArticlePage = () => {
-    const { articleId } = useParams();
-    const article = articles.find(article => article.name === articleId);
+  const { articleId } = useParams();
+  const article = articles.find((article) => article.name === articleId);
 
-    return (
-        <>
-        <h1>{article.title}</h1>
-        {article.content.map(paragraph => (
-            <p>{paragraph}</p>
-        ))}
-        </>
-    );
-}
+  return (
+    <>
+      <Link className='article-list-item' to='/articles'>
+        <IoIosArrowRoundBack /> Back
+      </Link>
+      <h1>{article.title}</h1>
+      {article.content.map((paragraph, idx) => (
+        <p key={idx}>{paragraph}</p>
+      ))}
+    </>
+  );
+};
 
 export default ArticlePage;
