5d3381298d9f83feebdbef1f23cad2839c2635ed
C. Felix Lee <felixlee.music@gmail.com>
2023-05-25 17:22:46 -0400

    setting up react router w/ browserRouter, routes, and route components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/About";
+import ArticleListPage from "./pages/ArticleListPage";
+import ArticlePage from "./pages/ArticlePage";
 
 function App() {
   return (
-    <div className="App">
-      <h1>My Awesome Blog</h1>
-      <div id="page-body">
-        Welcome to my blog!
+    <BrowserRouter>
+      <div className='App'>
+        <h1>The Big Idea Blog</h1>
+        <div id='page-body'>
+          Welcome to my blog!
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/about' element={<AboutPage />} />
+            <Route path='/articles' element={<ArticleListPage />} />
+            <Route path='/article/:articleId' element={<ArticlePage />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/ArticlePage.js
@@ -0,0 +1,5 @@
+const ArticlePage = () => {
+  return <h1>This is the Article page...</h1>;
+};
+
+export default ArticlePage;
