cce73a9ed05e6fb30b4192929c61dbf5cef3f1e5
C. Felix Lee <felixlee.music@gmail.com>
2023-05-27 00:06:52 -0400

    lesson 02_05: upvoting articles, backend api route

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,35 @@
 import express from "express";
 
+//stand-in db
+let articlesInfo = [
+  {
+    name: "moon-in-aquarius",
+    upvotes: 0,
+  },
+  {
+    name: "sun-in-aquarius-7th-house",
+    upvotes: 0,
+  },
+  {
+    name: "leo-rising-sun-opposite",
+    upvotes: 0,
+  },
+];
+
 const app = express();
 
 app.use(express.json());
 
-app.post("/hello", (req, res) => {
-  console.log(req.params);
-  res.send(`heyya, ${req.body.name}`);
-});
-
-app.get("/hello/:name", (req, res) => {
+//upvote api endpoint
+app.put("/api/articles/:name/upvote", (req, res) => {
   const { name } = req.params;
-  res.send(`hello ${name}!!!`);
+  const article = articlesInfo.find((a) => a.name === name);
+  if (article) {
+    article.upvotes += 1;
+    res.send(`the article has ${article.upvotes} ${article.upvotes > 1 || article.upvotes === 0 ? "upvotes" : "upvote"}`);
+  } else {
+    res.send(`That article doesn't exist`);
+  }
 });
 
 app.listen(8080, () => {
