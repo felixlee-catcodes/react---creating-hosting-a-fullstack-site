196f8dc50d46f263b0d08ffeb27b61f33f6ed779
C. Felix Lee <felixlee.music@gmail.com>
2023-05-26 23:46:24 -0400

    URL parameters in express

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,8 +5,13 @@ const app = express();
 app.use(express.json());
 
 app.post("/hello", (req, res) => {
-  console.log(req.body);
+  console.log(req.params);
   res.send(`heyya, ${req.body.name}`);
+});
+
+app.get("/hello/:name", (req, res) => {
+  const { name } = req.params;
+  res.send(`hello ${name}!!!`);
 });
 
 app.listen(8080, () => {
