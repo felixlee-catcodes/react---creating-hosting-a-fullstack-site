ceeea50300c57dc8d7bad4b119b6ef7420349b4d
C. Felix Lee <felixlee.music@gmail.com>
2023-05-30 18:26:10 -0400

    creating an add comment form, using state and passing props btwn parent and child components

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -4,6 +4,7 @@ import axios from "axios";
 //import { articles } from "./article-content.js";
 import NotFoundPage from "./NotFoundPage.js";
 import CommentsList from "../components/CommentsList.js";
+import AddCommentForm from "../components/AddCommentForm.js";
 
 const ArticlePage = () => {
   const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [], title: "", content: [] });
@@ -40,7 +41,7 @@ const ArticlePage = () => {
       {articleInfo.content.map((paragraph, i) => (
         <p key={i}>{paragraph}</p>
       ))}
-
+      <AddCommentForm articleName={articleId} onArticleUpdated={(updatedArticle) => setArticleInfo(updatedArticle)} />
       <CommentsList comments={articleInfo.comments} />
     </>
   );
